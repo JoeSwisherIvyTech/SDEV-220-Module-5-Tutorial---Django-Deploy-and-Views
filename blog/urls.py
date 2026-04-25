a9f96398b4e35a344597454a1b200f91d88ae7f3
Joe Swisher <jswisher6@ivytech.edu>
2026-04-25 15:03:04 -0400

    added forms

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 # create a list of url patterns that ties urls to function calls
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
